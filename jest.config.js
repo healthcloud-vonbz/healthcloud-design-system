c148f996f5070ffb077d5583531af57c6d85711a
brunoBrian <bruno.brian@vemprojuca.com>
2025-02-28 20:00:06 -0300

    test adjust

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-export default {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1", // Se usar alias (@), ajuste conforme necessário
-  },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
