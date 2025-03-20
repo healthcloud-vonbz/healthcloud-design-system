9c21e86b97c54b22d3af7e99dd8c969fce5a8bb5
brunoBrian <bruno.brian@vemprojuca.com>
2025-03-20 19:50:49 -0300

    fix: allow remove selected item from combobox

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         link: 'text-ds_health-primary underline-offset-4 hover:underline font-semibold',
         black: 'text-black hover:text-ds_health-hover-primary font-semibold',
         input:
-          'border border-input text-left font-thin bg-background hover:border-input hover:bg-accent hover:text-ds_health-hover-text-primary my-1 w-full text-left p-4 justify-normal',
+          'border border-input text-left font-thin bg-background hover:border-input hover:bg-accent hover:text-ds_health-hover-text-primary my-1 w-full text-left p-4 justify-normal my-0',
       },
       size: {
         default: 'h-10 px-4 py-2',
